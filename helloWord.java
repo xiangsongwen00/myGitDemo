d5ad95cfc4642e7c3b7a5256e1ebe2f5450a0371
yangjie <2409479323@qq.com>
2024-05-13 15:44:09 +0800

    hello YangJie

--- helloWord.java
+++ helloWord.java
@@ -1,5 +1,11 @@
 public class helloWord {
+    private static String yj="yangJie";
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String user=getYj();
+        System.out.println("Hello,"+user+" welcome to the World!");
+
+    }
+    private static String getYj(){
+        return yj;
     }
 }
